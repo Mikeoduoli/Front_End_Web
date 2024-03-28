4240ec3e6e2096f3d43684c8439575737cbc5bd0
Mikeoduoli <mikeoduoli@gmail.com>
2024-03-28 10:29:13 +0300

    Function Definition

---
+++ Meta_frontend/Week1/Loop_constructs.js
@@ -0,0 +1,37 @@
+// for (var i = 0; i <= 3; i++) {
+//     console.log("This is line ", i)
+// }
+
+
+// While Loops
+
+var j = 6;
+
+while (j < 40) {
+    console.log(j);
+    j += 1;
+}
+
+// Nested Loop
+for(i = 2024; i < 2026; i++){
+    console.log("")
+    for (j = 6; j < 9; j++) {
+    console.log("The year is " + i + " month " + j);
+    }
+}
+
+/*
+*
+*/
+for(var i = 0; i <= 5; i++) {
+    console.log("Hello");
+  }
+
+  var i = 3;
+  var j = 5;
+
+  if(i == 3 && j < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
---
+++ Meta_frontend/week2/Functions.js
@@ -0,0 +1,8 @@
+// Working with Functions
+
+function addNums(a, b) {
+    var c = a +b;
+    console.log(c);
+}
+
+addNums(5, 5);

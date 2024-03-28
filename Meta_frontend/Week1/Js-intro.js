b5338fb3463bdf94b3c23bd013191f991f740dd4
Mikeoduoli <mikeoduoli@gmail.com>
2024-03-28 08:30:37 +0300

    Conditional Statements

---
+++ Meta_frontend/Week1/Js-intro.js
@@ -0,0 +1,10 @@
+//  if...else statement
+
+var result = 50;
+
+if (result > 40) {
+    console.log("Congratulations, you passed.")
+}
+else {
+    console.log("You have the room to improve.")
+}
---
+++ Meta_frontend/Week1/Swicth.js
@@ -0,0 +1,17 @@
+// Working with Switch Statement
+
+var place = 'first';
+
+switch(place) {
+    case 'first':
+        console.log("Gold");
+        break;
+    case 'second':
+        console.log("Silver");
+        break;
+    case 'third':
+        console.log("Bronze");
+        break;
+    default:
+        console.log("No more medals.")
+}

a508ad9e47d1975396e64db8f31c5633e7ebb921
Mikeoduoli <mikeoduoli@gmail.com>
2024-03-29 23:06:27 +0300

    Functional Programming

--- Meta_frontend/week2/Functions.js
+++ Meta_frontend/week2/Functions.js
@@ -6,3 +6,65 @@ function addNums(a, b) {
 }
 
 addNums(5, 5);
+
+
+var result = null;
+console.log(result);
+
+
+try {
+    console.log('Hello');
+  } catch(err) {
+    console.log('Goodbye');
+  }
+
+  var x;
+
+  if(x === null) {
+    console.log("null");
+  } else if(x === undefined) {
+    console.log("undefined");
+  } else {
+    console.log("ok");
+  }
+
+//   try {
+//     console.log('hello)
+//     } catch(e) {
+//     console.log('caught')
+//     }
+
+
+    var bicycle = {
+        wheels: 2,
+        start: function() {
+  
+        },
+        stop: function() {
+  
+        }
+    };
+
+
+    try {
+        throw new Error();
+        console.log('Hello');
+      } catch(err) {
+        console.log('Goodbye');
+      }
+
+      
+  function add(a, b) {
+    console.log(a + b)
+  }
+
+  add(3, "4");
+
+  var str = "Hello";
+str.match("jello");
+
+try {
+    Number(5).toPrecision(300)
+    } catch(e) {
+    console.log("There was an error")
+    }

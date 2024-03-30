2d45ed22884e6416312112cb7453de6b01b12e93
Mikeoduoli <mikeoduoli@gmail.com>
2024-03-30 09:10:11 +0300

    Assignment on Working with Functions

--- Meta_frontend/week3/functional_programming.js
+++ Meta_frontend/week3/functional_programming.js
@@ -9,3 +9,19 @@ function convertCurrency(amount, rate) {
 currencyTwo = convertCurrency(currencyOne, exchangeRate);
 
 console.log(currencyTwo);
+
+// Task 1: Build a function-based console log message generator
+function consoleStyler(color, background, fontSize, txt) {
+
+    var message = "%c" + txt;
+
+    var style = `color: ${color};`
+
+    style += `background: ${background};`
+
+    style += `fontSize: ${fontSize};`
+    
+    console.log(`message`, `style`);
+}
+
+consoleStyler(`message`);

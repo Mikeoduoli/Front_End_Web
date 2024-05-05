9f48131256a673ee6f6f37474238450246faceb7
Mikeoduoli <mikeoduoli@gmail.com>
2024-05-05 20:29:55 +0300

    Stateful Components

--- Meta_frontend/5-react_basics/week_2/src/App.js
+++ Meta_frontend/5-react_basics/week_2/src/App.js
@@ -2,13 +2,21 @@
 import './App.css';
 // import ModeToggle from './ModeToggle';
 import Child from './Child';
+import React, { useState } from 'react';
 
 function App() {
   const date = new Date();
+  const[word, setWord] = useState('Eat');
+
+  function handleClick() {
+    setWord('Drink');
+  }
   
   return (
     <div>
       <Child message= {date.toLocaleTimeString()}/>
+      <h1>{word} at Little Lemon!</h1>
+      <button onClick={handleClick}>Click To Update.</button>
     </div>
   );
 }

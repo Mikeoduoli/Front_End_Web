eae12e48f36513ef93cffe1f881d4f88f9f9190b
Mikeoduoli <mikeoduoli@gmail.com>
2024-05-22 05:05:51 +0300

    useReducer Hook

--- html_css_JS-Playground/script.js
+++ html_css_JS-Playground/script.js
@@ -1,7 +1,7 @@
 console.log("Working Fetch API");
 
-fetch('https://randomuser.me/api/?results=1')
+fetch('https://randomuser.me/api/')
     .then(response => response.json())
     .then(data => console.log(data));
 
-console.log('Our Valued Customer, Please give us a moment while I get some information back from the Records Dept')
+console.log("Our Valued Customer, Please give us a moment while I get some information back from the Records Dept")
